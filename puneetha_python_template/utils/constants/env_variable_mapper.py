e8cb142b8a2afe33965aea84d87defe8bdb28632
puneethabm <puneetha.bm14@gmail.com>
2021-09-06 02:42:44 +0100

    fix typo

--- puneetha_python_template/utils/constants/env_variable_mapper.py
+++ puneetha_python_template/utils/constants/env_variable_mapper.py
@@ -11,11 +11,11 @@ class EnvironmentVariableMapper(Enum):
     # Options can be "DEV", "TEST", "PREPROD", "PROD"
     PBM_ENV_NAME = env.str("PBM_ENV_NAME", default="DEV")
 
-    PBM_MODULE_NAME = env.str("PBM_MODULE_NAME", default="pbm_python_template")
+    PBM_MODULE_NAME = env.str("PBM_MODULE_NAME", default="puneetha_python_template")
 
     PBM_LOG_CONFIG_FILE_NAME = env.str("PBM_LOG_CONFIG_FILE_NAME", default="logging.conf")
 
-    PBM_LOG_FILE_PATH = env.str("PBM_LOG_FILE_PATH", default="/var/log/pbm-python-template.log")
+    PBM_LOG_FILE_PATH = env.str("PBM_LOG_FILE_PATH", default="/var/log/puneetha-python-template.log")
 
     # Options can be "INFO", "DEBUG", "WARNING", "ERROR"
     LOGURU_LEVEL = env.str("LOGURU_LEVEL", default="INFO")
